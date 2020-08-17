070d341e77a2b5cb8dc9ebb63ae189096f313eea
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-08-17 09:37:48 +0100

    Put the input text back in the page on the button click, for now

--- src/cljs/kids_thing/core.cljs
+++ src/cljs/kids_thing/core.cljs
@@ -7,6 +7,8 @@
  (js/document.getElementById "submit-button")
  "click"
  (fn [event]
-   (println
-    (.-value (js/document.getElementById "input"))))
+   (let [input (.-value (js/document.getElementById "input"))]
+     (set!
+      (.-innerHTML (js/document.getElementById "response"))
+      input)))
  false)
