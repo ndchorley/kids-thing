181dccdd74194c3aa96d0a95197e4e3a520ca145
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-08-22 16:02:48 +0100

    Implement file and directory uploading

--- upload.py
+++ upload.py
@@ -1,21 +1,33 @@
 import ftplib
 import os
 
-host = ""
-user = ""
-password = ""
-
-ftp = ftplib.FTP(host)
-ftp.login(user=user, passwd=password)
-ftp.quit()
-
-def list_files(directory):
+def upload_files(directory, ftp):
     with os.scandir(directory) as it:
         for entry in it:
             if entry.is_file():
-                print(entry.path)
+                print("Uploading file " + entry.path)
+                with open(entry.path, "rb") as f:
+                    ftp.storbinary("STOR " + entry.path.replace("target/", ""), f, blocksize=32768)
             else:
-                print(entry.name + "/")
-                list_files(entry.path)
+                name = entry.path.replace("target/", "")
+                print("Creating directory " + name + "/")
+                try:
+                    ftp.mkd(name)
+                except:
+                    pass
 
-list_files("target")
+                upload_files(entry.path, ftp)
+
+
+host = ""
+user = ""
+password = ""
+server_dir = ""
+
+ftp = ftplib.FTP(host)
+ftp.login(user=user, passwd=password)
+ftp.cwd(server_dir)
+
+upload_files("target", ftp)
+
+ftp.quit()
