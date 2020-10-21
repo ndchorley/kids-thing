7afcf5851c9ba1c7760b2e4c0e86d869f506c4aa
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-10-21 08:33:18 +0100

    Hard code the first question in the page

--- src/cljs/kids_thing/core.cljs
+++ src/cljs/kids_thing/core.cljs
@@ -22,11 +22,3 @@
  respond
  false)
 
-(.addEventListener
- js/document
- "DOMContentLoaded"
- (fn [event]
-   (set!
-    (.-innerHTML (js/document.getElementById "question"))
-    "<h1>What is your name?</h1>"))
- false)
