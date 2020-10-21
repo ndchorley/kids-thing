3068adf0a5702a7d6a0c425f1ffeb3d710b76c23
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-10-21 08:56:14 +0100

    Get response to a question/answer from a table

--- src/cljs/kids_thing/core.cljs
+++ src/cljs/kids_thing/core.cljs
@@ -5,14 +5,26 @@
 (defn next-mode [current-mode]
   (if (= current-mode :respond) :next :respond))
 
+(defn get-question [] (.-innerHTML (js/document.getElementById "question")))
+
+(defn get-answer [] (.-value (js/document.getElementById "input")))
+
+(defn get-response []
+  (def questions-to-responses
+    {"What is your name?" (fn [answer] (str "Hi " answer))})
+
+  (let [answer (get-answer)
+        question (get-question)
+        respond (questions-to-responses question)]
+    (respond answer)))
+
 (defn respond [event]
   (set! (.-textContent (js/document.getElementById "submit-button"))
         (if (= (deref mode) :respond) "Next" "Click me"))
 
-  (let [input (.-value (js/document.getElementById "input"))]
-    (set!
-     (.-innerHTML (js/document.getElementById "response"))
-     (str "Hi " input)))
+  (set!
+   (.-innerHTML (js/document.getElementById "response"))
+   (get-response))
 
   (swap! mode next-mode))
 
