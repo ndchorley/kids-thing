ca6ce09b3582a0308f647bea66b3083c120a616d
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-11-21 11:04:46 +0000

    Capitalise the name

--- src/cljs/kids_thing/responses.cljs
+++ src/cljs/kids_thing/responses.cljs
@@ -1,14 +1,15 @@
-(ns kids-thing.responses)
+(ns kids-thing.responses (:require [clojure.string :as string]))
 
 (defn respond-to-name [name]
-  (let [hour-of-day (.getHours (new js/Date))]
+  (let [hour-of-day (.getHours (new js/Date))
+        capitalised-name (string/capitalize name)]
     (cond
-      (< hour-of-day 12) {:text (str "Good morning " name)
+      (< hour-of-day 12) {:text (str "Good morning " capitalised-name)
                           :image "https://live.staticflickr.com/7491/16296571715_b3ebde1e38_b.jpg"}
 
-      (< hour-of-day 17) {:text (str "Good afternoon " name)
+      (< hour-of-day 17) {:text (str "Good afternoon " capitalised-name)
                           :image "https://live.staticflickr.com/65535/49611913502_dde5b04bbe_b.jpg"}
-      true {:text (str "Good evening " name)
+      true {:text (str "Good evening " capitalised-name)
             :image "https://live.staticflickr.com/65535/48763561263_6707d9fcb2_b.jpg"})))
 
 (def questions-to-responses
